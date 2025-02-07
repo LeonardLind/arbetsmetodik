1e9553249f9f948b8f55581c64b167f054f319db
Michael <mjhaddis@gmail.com>
2025-02-07 12:21:17 +0100

    Added hero section

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const textContainer = document.getElementById("hero-text");
+  const imageContainer = document.getElementById("hero-img");
+
+  const heading = document.createElement("h1");
+  heading.innerHTML = "Skapa framtidens webb med oss!"
+
+  const paragraph = document.createElement("p");
+  paragraph.innerHTML = "Vi är en kreativ webbyrå som kombinerar design och teknik för att bygga unika digitala upplevelser.";
+
+  const button = document.createElement("button");
+  button.href = "#";
+  button.innerHTML = "Kontakta oss"
+
+  textContainer.appendChild(heading);
+  textContainer.appendChild(paragraph);
+  textContainer.appendChild(button);
+
+  const image = document.createElement("img");
+  image.src = "Hero-image.png";
+  image.alt = "Webbyråteam som arbetar kreativt";
+
+  imageContainer.appendChild(image);
+})
+
 const jobs = [
   {
     title: "Frontendutvecklare",
